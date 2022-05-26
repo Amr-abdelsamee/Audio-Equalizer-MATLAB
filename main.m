2af96693a25d332a9c5b4736ff94d49bb594afe2
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-05-27 00:27:28 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,35 +1,45 @@
-[filename,path] = uigetfile({'*.wav'});
-fullPath = strcat(path,filename);
+
+function main()
+
+
+file = Read_file();
+
+[Y, fs] = wavread(file);
 
 gain = input('What is the Gain: ');
-
-gain
 
 filter_type = input('select: 1-FIR , 2-IIR: ');
 while filter_type ~= 1 && filter_type ~=2
     filter_type = input('Incorrect response! select: 1-FIR , 2-IIR: ');
 end
 
-filter_type
-
 Fs= input('Enter Output Sample Rate: ');
 
 
 
+% play the sound
+% sound(Y,fs) % to stop the sound type :" clear sound " in matlab console
+
+end
 
 
 
+function file = Read_file()
+
+[filename,path] = uigetfile({'*.wav'});
+
+file = strcat(path,filename); % file fullPath
+end
 
 
 
+function BandFilters()
 
 
-
-
-
-
-
-
+bandpass(song,[230 450],fs)
+ 
+ 
+end
 
 
 
